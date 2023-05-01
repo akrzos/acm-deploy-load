ec6816f944bf8b835f1828ed1d84471001a2ce5a
Sherin Varughese <sherin.a.varughese@gmail.com>
2023-05-01 14:22:01 -0400

    add postgres db envVars (#3)
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>

--- patch/acm-2.8.sh
+++ patch/acm-2.8.sh
@@ -11,6 +11,10 @@ echo "Applying ACM search-v2-operator database resources bump"
 oc patch search -n open-cluster-management search-v2-operator --type json -p '[{"op": "add", "path": "/spec/deployments/database/resources", "value": {"limits": {"memory": "16Gi"}, "requests": {"memory": "32Mi", "cpu": "25m"}}}]'
 echo "Sleep 10"
 sleep 10
+echo "Applying ACM search-v2-operator database envvars"
+oc patch search -n open-cluster-management search-v2-operator --type json -p '[{"op": "add", "path": "/spec/deployments/database/envVar", "value": [{"name": "POSTGRESQL_EFFECTIVE_CACHE_SIZE", "value": "1024MB"}, {"name": "POSTGRESQL_SHARED_BUFFERS", "value": "512MB"}, {"name": "WORK_MEM", "value": "128MB"}]}]'
+echo "Sleep 10"
+sleep 10
 echo "Applying ACM search-v2-operator indexer resources bump"
 oc patch search -n open-cluster-management search-v2-operator --type json -p '[{"op": "add", "path": "/spec/deployments/indexer/resources", "value": {"limits": {"memory": "4Gi"}, "requests": {"memory": "128Mi", "cpu": "25m"}}}]'
 echo "Sleep 10"
