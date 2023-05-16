21194bd3361cd15a683fcf5b4afffa236c82fbd2
Alex Krzos <akrzos@redhat.com>
2023-05-15 22:55:56 -0400

    Merge Complete and Operator upgrade setup as well

---
+++ ansible/roles/rhacm-ztp-complete-upgrade-setup/templates/patch-cgu-complete.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+{% for cgu in range(((ztp_done_clusters.stdout_lines | length) / clusters_per_cgu) | round(0, 'ceil') | int) %}
+date -u
+oc --namespace=ztp-platform-upgrade patch clustergroupupgrade.ran.openshift.io/complete-upgrade-{{ du_upgrade_version | replace('.', '-') }}-{{ '%04d' | format(cgu) }} --patch '{"spec":{"enable":true,"remediationStrategy":{"timeout":{{ complete_upgrade_patch_sleep }} } } }' --type=merge
+{% if not loop.last %}
+sleep {{ (complete_upgrade_patch_sleep + complete_upgrade_patch_offset) * 60 }}
+{% endif %}
+{% endfor %}
---
+++ ansible/roles/rhacm-ztp-operator-upgrade-setup/templates/patch-cgu-operator.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+{% for cgu in range(((operator_update_clusters.stdout_lines | length) / clusters_per_cgu) | round(0, 'ceil') | int) %}
+oc --namespace=ztp-operator-upgrade patch clustergroupupgrade.ran.openshift.io/operator-upgrade-{{ disconnected_operator_index_tag | replace('.', '') }}-{{ '%04d' | format(cgu) }} --patch '{"spec":{"enable":true}}' --type=merge
+{% endfor %}
