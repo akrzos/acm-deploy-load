0bd22547d9e34ce3f3f0c93b2e415a937e6c1f18
Alex Krzos <akrzos@redhat.com>
2023-11-29 21:55:16 -0500

    More stats collected and analyzed regarding ansiblejobs

--- acm-deploy-load/analyze-ansiblejobs.py
+++ acm-deploy-load/analyze-ansiblejobs.py
@@ -44,7 +44,7 @@ def main():
   logger.info("Analyze ansiblejobs")
   ts = datetime.now().strftime("%Y%m%d-%H%M%S")
   aj_csv_file = "{}/ansiblejobs-{}.csv".format(cliargs.results_directory, ts)
-  # aj_stats_file = "{}/ansiblejobs-{}.stats".format(cliargs.results_directory, ts)
+  aj_stats_file = "{}/ansiblejobs-{}.stats".format(cliargs.results_directory, ts)
 
   oc_cmd = ["oc", "get", "ansiblejobs", "-A", "-o", "json"]
   rc, output = command(oc_cmd, False, retries=3, no_log=True)
@@ -53,6 +53,12 @@ def main():
     sys.exit(1)
   aj_data = json.loads(output)
 
+  jobs_analyzed = len(aj_data["items"])
+  jobs_status = {}
+  create_started_durations = []
+  started_finished_durations = []
+  complete_durations = []
+
   logger.info("Writing CSV: {}".format(aj_csv_file))
   with open(aj_csv_file, "w") as csv_file:
     csv_file.write("name,tower_id,target_count,status,changed,failed,elapsed,creationTimestamp,started,finished,complete_duration,create_started_duration,started_finished_duration\n")
@@ -60,22 +66,49 @@ def main():
   for item in aj_data["items"]:
     aj_name = item["metadata"]["name"]
     aj_creationTimestamp = item["metadata"]["creationTimestamp"]
-    aj_tower_id = item["metadata"]["labels"]["tower_job_id"]
+    aj_tower_id = ""
+    if "labels" in item["metadata"] and "tower_job_id" in item["metadata"]["labels"]:
+      aj_tower_id = item["metadata"]["labels"]["tower_job_id"]
     aj_target_count = len(item["spec"]["extra_vars"]["target_clusters"])
 
-    aj_result_changed = item["status"]["ansibleJobResult"]["changed"]
-    aj_result_status = item["status"]["ansibleJobResult"]["status"]
-    aj_result_failed = item["status"]["ansibleJobResult"]["failed"]
-    aj_result_started = item["status"]["ansibleJobResult"]["started"]
-    aj_result_finished = item["status"]["ansibleJobResult"]["finished"]
-    aj_result_elapsed = item["status"]["ansibleJobResult"]["elapsed"]
+    aj_result_changed = ""
+    aj_result_status = ""
+    aj_result_failed = ""
+    aj_result_started = ""
+    aj_result_finished = ""
+    aj_result_elapsed = ""
+
+    if "changed" in item["status"]["ansibleJobResult"]:
+      aj_result_changed = item["status"]["ansibleJobResult"]["changed"]
+    if "status" in item["status"]["ansibleJobResult"]:
+      aj_result_status = item["status"]["ansibleJobResult"]["status"]
+      if aj_result_status in jobs_status:
+        jobs_status[aj_result_status] += 1
+      else:
+        jobs_status[aj_result_status] = 1
+    if "failed" in item["status"]["ansibleJobResult"]:
+      aj_result_failed = item["status"]["ansibleJobResult"]["failed"]
+    if "started" in item["status"]["ansibleJobResult"]:
+      aj_result_started = item["status"]["ansibleJobResult"]["started"]
+    if "finished" in item["status"]["ansibleJobResult"]:
+      aj_result_finished = item["status"]["ansibleJobResult"]["finished"]
+    if "elapsed" in item["status"]["ansibleJobResult"]:
+      aj_result_elapsed = item["status"]["ansibleJobResult"]["elapsed"]
 
     created_dt = datetime.strptime(aj_creationTimestamp, "%Y-%m-%dT%H:%M:%SZ")
-    started_dt = datetime.strptime(aj_result_started, "%Y-%m-%dT%H:%M:%S.%fZ")
-    finished_dt = datetime.strptime(aj_result_finished, "%Y-%m-%dT%H:%M:%S.%fZ")
-    complete_duration = (finished_dt - created_dt).total_seconds()
-    create_started_duration = (started_dt - created_dt).total_seconds()
-    started_finished_duration = (finished_dt - started_dt).total_seconds()
+    create_started_duration = ""
+    started_finished_duration = ""
+    complete_duration = ""
+    if aj_result_started != "":
+      started_dt = datetime.strptime(aj_result_started, "%Y-%m-%dT%H:%M:%S.%fZ")
+      create_started_duration = (started_dt - created_dt).total_seconds()
+      create_started_durations.append(create_started_duration)
+    if aj_result_finished != "":
+      finished_dt = datetime.strptime(aj_result_finished, "%Y-%m-%dT%H:%M:%S.%fZ")
+      started_finished_duration = (finished_dt - started_dt).total_seconds()
+      complete_duration = (finished_dt - created_dt).total_seconds()
+      started_finished_durations.append(started_finished_duration)
+      complete_durations.append(complete_duration)
 
     with open(aj_csv_file, "a") as csv_file:
       csv_file.write("{},{},{},{},{},{},{},{},{},{},{},{},{}\n".format(
@@ -83,35 +116,44 @@ def main():
           aj_result_elapsed, aj_creationTimestamp, aj_result_started, aj_result_finished, complete_duration,
           create_started_duration, started_finished_duration))
 
-  # Stats oin jobs not determined yet
-  # logger.info("Writing Stats: {}".format(aj_stats_file))
-  # stats_count = len(aci_installcompleted_values)
-  # stats_min = 0
-  # stats_avg = 0
-  # stats_50p = 0
-  # stats_95p = 0
-  # stats_99p = 0
-  # stats_max = 0
-  # if stats_count > 0:
-  #   stats_min = np.min(aci_installcompleted_values)
-  #   stats_avg = round(np.mean(aci_installcompleted_values), 1)
-  #   stats_50p = round(np.percentile(aci_installcompleted_values, 50), 1)
-  #   stats_95p = round(np.percentile(aci_installcompleted_values, 95), 1)
-  #   stats_99p = round(np.percentile(aci_installcompleted_values, 99), 1)
-  #   stats_max = np.max(aci_installcompleted_values)
-  #
-  # with open(aj_stats_file, "w") as stats_file:
-  #   log_write(stats_file, "Stats only on AgentClusterInstall CRs in InstallationCompleted")
-  #   log_write(stats_file, "Count: {}".format(stats_count))
-  #   log_write(stats_file, "Min: {}".format(stats_min))
-  #   log_write(stats_file, "Average: {}".format(stats_avg))
-  #   log_write(stats_file, "50 percentile: {}".format(stats_50p))
-  #   log_write(stats_file, "95 percentile: {}".format(stats_95p))
-  #   log_write(stats_file, "99 percentile: {}".format(stats_99p))
-  #   log_write(stats_file, "Max: {}".format(stats_max))
+  logger.info("Writing Stats: {}".format(aj_stats_file))
+  with open(aj_stats_file, "w") as stats_file:
+    log_write(stats_file, "Analyzed {} ansiblejobs".format(jobs_analyzed))
+    for status in sorted(jobs_status.keys()):
+      log_write(stats_file, "AnsibleJobs with {} status: {}".format(status, jobs_status[status]))
+    log_write(stats_file, "#############################################")
+    log_write(stats_file, "Created to Started Duration Stats")
+    log_write(stats_file, "Count: {}".format(len(create_started_durations)))
+    if len(create_started_durations) > 0:
+      log_write(stats_file, "Min: {}".format(np.min(create_started_durations)))
+      log_write(stats_file, "Average: {}".format(round(np.mean(create_started_durations), 1)))
+      log_write(stats_file, "50 percentile: {}".format(round(np.percentile(create_started_durations, 50), 1)))
+      log_write(stats_file, "95 percentile: {}".format(round(np.percentile(create_started_durations, 95), 1)))
+      log_write(stats_file, "99 percentile: {}".format(round(np.percentile(create_started_durations, 99), 1)))
+      log_write(stats_file, "Max: {}".format(np.max(create_started_durations)))
+    log_write(stats_file, "#############################################")
+    log_write(stats_file, "Started to Finished Duration Stats")
+    log_write(stats_file, "Count: {}".format(len(started_finished_durations)))
+    if len(create_started_durations) > 0:
+      log_write(stats_file, "Min: {}".format(np.min(started_finished_durations)))
+      log_write(stats_file, "Average: {}".format(round(np.mean(started_finished_durations), 1)))
+      log_write(stats_file, "50 percentile: {}".format(round(np.percentile(started_finished_durations, 50), 1)))
+      log_write(stats_file, "95 percentile: {}".format(round(np.percentile(started_finished_durations, 95), 1)))
+      log_write(stats_file, "99 percentile: {}".format(round(np.percentile(started_finished_durations, 99), 1)))
+      log_write(stats_file, "Max: {}".format(np.max(started_finished_durations)))
+    log_write(stats_file, "#############################################")
+    log_write(stats_file, "Complete Duration Stats")
+    log_write(stats_file, "Count: {}".format(len(complete_durations)))
+    if len(create_started_durations) > 0:
+      log_write(stats_file, "Min: {}".format(np.min(complete_durations)))
+      log_write(stats_file, "Average: {}".format(round(np.mean(complete_durations), 1)))
+      log_write(stats_file, "50 percentile: {}".format(round(np.percentile(complete_durations, 50), 1)))
+      log_write(stats_file, "95 percentile: {}".format(round(np.percentile(complete_durations, 95), 1)))
+      log_write(stats_file, "99 percentile: {}".format(round(np.percentile(complete_durations, 99), 1)))
+      log_write(stats_file, "Max: {}".format(np.max(complete_durations)))
 
   end_time = time.time()
-  logger.info("Took {}s".format(round(end_time - start_time, 1)))
+  logger.info("Analysis Took {}s".format(round(end_time - start_time, 1)))
 
 if __name__ == "__main__":
   sys.exit(main())
