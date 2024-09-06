bee9d01082dc0d2576b990bc9a288c4c5a0e43c5
Alex Krzos <akrzos@redhat.com>
2024-09-06 12:35:01 -0400

    Adjust IBI script

--- patch/ibi-2.12.sh
+++ patch/ibi-2.12.sh
@@ -3,15 +3,16 @@
 
 export KUBECONFIG=/root/bm/kubeconfig
 
-# echo "Patching MCE IBIO container image"
+echo "Patching MCE IBIO container image"
 # oc image mirror -a /opt/registry/pull-secret-disconnected.txt quay.io/itsoiref/image-based-install-operator:cabundle e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/ibio/image-based-install-operator:cabundle --keep-manifest-list --continue-on-error=true
+# oc image mirror -a /opt/registry/pull-secret-disconnected.txt quay.io/itsoiref/image-based-install-operator:fixes e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/ibio/image-based-install-operator:fixes-03 --keep-manifest-list --continue-on-error=true
 oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] | select(.name=="manager").image'
 oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] | select(.name=="manager").command[]'
 oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] | select(.name=="server").image'
 oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] | select(.name=="server").command[]'
 oc annotate multiclusterengine multiclusterengine pause=true
-oc get deploy -n multicluster-engine image-based-install-operator -o json |  jq '.spec.template.spec.containers[] |= (select(.name=="manager").image = "e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/ibio/image-based-install-operator:cabundle")' | oc replace -f -
-oc get deploy -n multicluster-engine image-based-install-operator -o json |  jq '.spec.template.spec.containers[] |= (select(.name=="server").image = "e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/ibio/image-based-install-operator:cabundle")' | oc replace -f -
+oc get deploy -n multicluster-engine image-based-install-operator -o json |  jq '.spec.template.spec.containers[] |= (select(.name=="manager").image = "e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/ibio/image-based-install-operator:fixes-03")' | oc replace -f -
+oc get deploy -n multicluster-engine image-based-install-operator -o json |  jq '.spec.template.spec.containers[] |= (select(.name=="server").image = "e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/ibio/image-based-install-operator:fixes-03")' | oc replace -f -
 oc get deploy -n multicluster-engine image-based-install-operator -o json |  jq '.spec.template.spec.containers[] |= (select(.name=="manager").command = ["/usr/local/bin/manager"])' | oc replace -f -
 oc get deploy -n multicluster-engine image-based-install-operator -o json |  jq '.spec.template.spec.containers[] |= (select(.name=="server").command = ["/usr/local/bin/server"])' | oc replace -f -
 oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] | select(.name=="manager").image'
