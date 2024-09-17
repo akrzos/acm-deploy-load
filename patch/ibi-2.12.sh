23e1a1e9ca8c3674c9e8a61b4d97f2cfea580e3e
Alex Krzos <akrzos@redhat.com>
2024-09-17 17:05:53 -0400

    Adjust the ibi patch for dataimage container

--- patch/ibi-2.12.sh
+++ patch/ibi-2.12.sh
@@ -4,21 +4,20 @@
 export KUBECONFIG=/root/bm/kubeconfig
 
 echo "Patching MCE IBIO container image"
-# oc image mirror -a /opt/registry/pull-secret-disconnected.txt quay.io/itsoiref/image-based-install-operator:cabundle e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/ibio/image-based-install-operator:cabundle --keep-manifest-list --continue-on-error=true
-# oc image mirror -a /opt/registry/pull-secret-disconnected.txt quay.io/itsoiref/image-based-install-operator:fixes e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/ibio/image-based-install-operator:fixes-03 --keep-manifest-list --continue-on-error=true
-oc image mirror -a /opt/registry/pull-secret-disconnected.txt quay.io/itsoiref/image-based-install-operator:fixes2 e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/ibio/image-based-install-operator:fixes2-01 --keep-manifest-list --continue-on-error=true
+# oc image mirror -a /opt/registry/pull-secret-disconnected.txt quay.io/itsoiref/image-based-install-operator:cabundle e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/ibio/image-based-install-operator:cabundle --keep-manifest-list
+# oc image mirror -a /opt/registry/pull-secret-disconnected.txt quay.io/itsoiref/image-based-install-operator:fixes e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/ibio/image-based-install-operator:fixes-03 --keep-manifest-list
+# oc image mirror -a /opt/registry/pull-secret-disconnected.txt quay.io/itsoiref/image-based-install-operator:fixes2 e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/ibio/image-based-install-operator:fixes2-01 --keep-manifest-list
+oc image mirror -a /opt/registry/pull-secret-disconnected.txt quay.io/itsoiref/image-based-install-operator:dataimage e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/ibio/image-based-install-operator:dataimage --keep-manifest-list
+
 oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] | select(.name=="manager").image'
-# oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] | select(.name=="manager").command[]'
 oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] | select(.name=="server").image'
-# oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] | select(.name=="server").command[]'
 oc annotate multiclusterengine multiclusterengine pause=true
-oc get deploy -n multicluster-engine image-based-install-operator -o json |  jq '.spec.template.spec.containers[] |= (select(.name=="manager").image = "e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/ibio/image-based-install-operator:fixes2-01")' | oc replace -f -
-oc get deploy -n multicluster-engine image-based-install-operator -o json |  jq '.spec.template.spec.containers[] |= (select(.name=="server").image = "e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/ibio/image-based-install-operator:fixes2-01")' | oc replace -f -
-# oc get deploy -n multicluster-engine image-based-install-operator -o json |  jq '.spec.template.spec.containers[] |= (select(.name=="manager").command = ["/usr/local/bin/manager"])' | oc replace -f -
-# oc get deploy -n multicluster-engine image-based-install-operator -o json |  jq '.spec.template.spec.containers[] |= (select(.name=="server").command = ["/usr/local/bin/server"])' | oc replace -f -
+oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] |= (select(.name=="manager").image = "e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/ibio/image-based-install-operator:dataimage")' | oc replace -f -
+oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] |= (select(.name=="server").image = "e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/ibio/image-based-install-operator:dataimage")' | oc replace -f -
 oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] | select(.name=="manager").image'
-# oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] | select(.name=="manager").command[]'
 oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] | select(.name=="server").image'
-# oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] | select(.name=="server").command[]'
 echo "Sleep 45"
 sleep 45
+
+# DataImage IBIO needs an additional patch to the clusterrole
+oc patch clusterrole open-cluster-management:image-based-install-operator:controller-manager --type json -p '[{"op": "add", "path": "/rules/-", "value": {"apiGroups": ["metal3.io"], "resources": ["baremetalhosts/finalizers"], "verbs": ["update"]}}, {"op": "add", "path": "/rules/-", "value": {"apiGroups": ["metal3.io"], "resources": ["dataimages"], "verbs": ["create", "delete", "get", "list", "patch", "update", "watch"]}}]'
