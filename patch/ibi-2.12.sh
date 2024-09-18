f1f79f37389933b1a3d8a4dee73190351e4f1eb9
Alex Krzos <akrzos@redhat.com>
2024-09-18 09:00:45 -0400

    Add Hive Controller patch

--- patch/ibi-2.12.sh
+++ patch/ibi-2.12.sh
@@ -9,15 +9,29 @@ echo "Patching MCE IBIO container image"
 # oc image mirror -a /opt/registry/pull-secret-disconnected.txt quay.io/itsoiref/image-based-install-operator:fixes2 e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/ibio/image-based-install-operator:fixes2-01 --keep-manifest-list
 oc image mirror -a /opt/registry/pull-secret-disconnected.txt quay.io/itsoiref/image-based-install-operator:dataimage e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/ibio/image-based-install-operator:dataimage --keep-manifest-list
 
+echo "Pausing MCE"
+oc annotate multiclusterengine multiclusterengine pause=true
 oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] | select(.name=="manager").image'
 oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] | select(.name=="server").image'
-oc annotate multiclusterengine multiclusterengine pause=true
 oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] |= (select(.name=="manager").image = "e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/ibio/image-based-install-operator:dataimage")' | oc replace -f -
 oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] |= (select(.name=="server").image = "e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/ibio/image-based-install-operator:dataimage")' | oc replace -f -
 oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] | select(.name=="manager").image'
 oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] | select(.name=="server").image'
-echo "Sleep 45"
-sleep 45
+echo "Sleep 15"
+sleep 15
 
 # DataImage IBIO needs an additional patch to the clusterrole
+echo "Patching IBIO clusterrole"
 oc patch clusterrole open-cluster-management:image-based-install-operator:controller-manager --type json -p '[{"op": "add", "path": "/rules/-", "value": {"apiGroups": ["metal3.io"], "resources": ["baremetalhosts/finalizers"], "verbs": ["update"]}}, {"op": "add", "path": "/rules/-", "value": {"apiGroups": ["metal3.io"], "resources": ["dataimages"], "verbs": ["create", "delete", "get", "list", "patch", "update", "watch"]}}]'
+
+# Patch hive-controller
+echo "Scaling Hive Operator to 0"
+oc scale -n multicluster-engine deploy/hive-operator --replicas=0
+echo "Sleep 15"
+sleep 15
+echo "Patching Hive Controller container image"
+oc get deploy -n hive hive-controllers -o json | jq '.spec.template.spec.containers[] | select(.name=="manager").image'
+oc get deploy -n hive hive-controllers -o json | jq '.spec.template.spec.containers[] |= (select(.name=="manager").image = "e38-h01-000-r650.rdu2.scalelab.redhat.com:5000/hive/hive:clusterinstall-update-order-03")' | oc replace -f -
+oc get deploy -n hive hive-controllers -o json | jq '.spec.template.spec.containers[] | select(.name=="manager").image'
+echo "Sleep 15"
+sleep 15
