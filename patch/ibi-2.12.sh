37128a54e9940a46cb7b663dcc3bb349d7495180
Alex Krzos <akrzos@redhat.com>
2024-10-09 11:06:29 -0400

    Adjust import controller memory

--- patch/ibi-2.12.sh
+++ patch/ibi-2.12.sh
@@ -41,6 +41,8 @@ sleep 15
 
 # Patch MCE managedcluster import controller Memory Limits(Requires MCE pause)
 echo "Patching MCE IBIO container image memory limits"
-oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[0].resources.limits.memory'
-oc get deploy -n multicluster-engine image-based-install-operator -o json |  jq '.spec.template.spec.containers[0].resources.limits.memory = "16Gi"' | oc replace -f -
-oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[0].resources.limits.memory'
+oc get deploy -n multicluster-engine managedcluster-import-controller-v2 -o json | jq '.spec.template.spec.containers[0].resources.limits.memory'
+oc get deploy -n multicluster-engine managedcluster-import-controller-v2 -o json |  jq '.spec.template.spec.containers[0].resources.limits.memory = "4Gi"' | oc replace -f -
+oc get deploy -n multicluster-engine managedcluster-import-controller-v2 -o json | jq '.spec.template.spec.containers[0].resources.limits.memory'
+echo "Sleep 15"
+sleep 15
