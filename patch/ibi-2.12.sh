70329b0d04212d9604668ec214c17d9e061b6781
Alex Krzos <akrzos@redhat.com>
2024-10-09 10:54:51 -0400

    Adjust ibi patches

--- patch/ibi-2.12.sh
+++ patch/ibi-2.12.sh
@@ -8,27 +8,20 @@ bastion=$(hostname)
 echo "Pausing MCE"
 oc annotate multiclusterengine multiclusterengine pause=true
 
+# Patch MCE IBIO container image (Requires MCE pause)
 echo "Patching MCE IBIO container image"
-# oc image mirror -a /opt/registry/pull-secret-bastion.txt quay.io/itsoiref/image-based-install-operator:cabundle ${bastion}:5000/ibio/image-based-install-operator:cabundle --keep-manifest-list
-# oc image mirror -a /opt/registry/pull-secret-bastion.txt quay.io/itsoiref/image-based-install-operator:fixes ${bastion}:5000/ibio/image-based-install-operator:fixes-03 --keep-manifest-list
-# oc image mirror -a /opt/registry/pull-secret-bastion.txt quay.io/itsoiref/image-based-install-operator:fixes2 ${bastion}:5000/ibio/image-based-install-operator:fixes2-01 --keep-manifest-list
-# oc image mirror -a /opt/registry/pull-secret-bastion.txt quay.io/itsoiref/image-based-install-operator:dataimage ${bastion}:5000/ibio/image-based-install-operator:dataimage --keep-manifest-list
-# DataImage IBIO needs an additional patch to the clusterrole
-# echo "Patching IBIO clusterrole"
-# oc patch clusterrole open-cluster-management:image-based-install-operator:controller-manager --type json -p '[{"op": "add", "path": "/rules/-", "value": {"apiGroups": ["metal3.io"], "resources": ["baremetalhosts/finalizers"], "verbs": ["update"]}}, {"op": "add", "path": "/rules/-", "value": {"apiGroups": ["metal3.io"], "resources": ["dataimages"], "verbs": ["create", "delete", "get", "list", "patch", "update", "watch"]}}]'
-# OOM image to address IBIO OOM at scale
-oc image mirror -a /opt/registry/pull-secret-bastion.txt quay.io/itsoiref/image-based-install-operator:oom ${bastion}:5000/ibio/image-based-install-operator:oom-03 --keep-manifest-list
+oc image mirror -a /opt/registry/pull-secret-bastion.txt quay.io/eranco74/image-based-install-operator:MGMT-19033 ${bastion}:5000/ibio/image-based-install-operator:MGMT-19033 --keep-manifest-list
 
 oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] | select(.name=="manager").image'
 oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] | select(.name=="server").image'
-oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] |= (select(.name=="manager").image = "'"${bastion}"':5000/ibio/image-based-install-operator:oom-03")' | oc replace -f -
-oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] |= (select(.name=="server").image = "'"${bastion}"':5000/ibio/image-based-install-operator:oom-03")' | oc replace -f -
+oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] |= (select(.name=="manager").image = "'"${bastion}"':5000/ibio/image-based-install-operator:MGMT-19033")' | oc replace -f -
+oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] |= (select(.name=="server").image = "'"${bastion}"':5000/ibio/image-based-install-operator:MGMT-19033")' | oc replace -f -
 oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] | select(.name=="manager").image'
 oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[] | select(.name=="server").image'
 echo "Sleep 15"
 sleep 15
 
-# Patch MCE IBIO Memory Limits (Needs MCE pause)
+# Patch MCE IBIO Memory Limits(Requires MCE pause)
 echo "Patching MCE IBIO container image memory limits"
 oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[0].resources.limits.memory'
 oc get deploy -n multicluster-engine image-based-install-operator -o json |  jq '.spec.template.spec.containers[0].resources.limits.memory = "16Gi"' | oc replace -f -
@@ -36,15 +29,18 @@ oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '
 echo "Sleep 15"
 sleep 15
 
-# Patch hive-controller container image (requires mce pause)
-# echo "Scaling Hive Operator to 0"
-# oc scale -n multicluster-engine deploy/hive-operator --replicas=0
-# echo "Sleep 15"
-# sleep 15
-# echo "Patching Hive Controller container image"
-# oc image mirror -a /opt/registry/pull-secret-bastion.txt quay.io/2uasimojo/hive:clusterinstall-update-order ${bastion}:5000/hive/hive:clusterinstall-update-order-03 --keep-manifest-list
-# oc get deploy -n hive hive-controllers -o json | jq '.spec.template.spec.containers[] | select(.name=="manager").image'
-# oc get deploy -n hive hive-controllers -o json | jq '.spec.template.spec.containers[] |= (select(.name=="manager").image = "'"${bastion}"':5000/hive/hive:clusterinstall-update-order-03")' | oc replace -f -
-# oc get deploy -n hive hive-controllers -o json | jq '.spec.template.spec.containers[] | select(.name=="manager").image'
-# echo "Sleep 15"
-# sleep 15
+# Patch MCE ocm-controller container image (Requires MCE pause)
+echo "Patching MCE ocm-controller container image"
+oc image mirror -a /opt/registry/pull-secret-bastion.txt quay.io/stolostron/multicloud-manager:2.12.0-PR781-e04efaf8338b31f21f76bfdfcb95df6c291a4ead ${bastion}:5000/stolostron/multicloud-manager:deletefix-01 --keep-manifest-list
+
+oc get deploy -n multicluster-engine ocm-controller -o json | jq '.spec.template.spec.containers[] | select(.name=="ocm-controller").image'
+oc get deploy -n multicluster-engine ocm-controller -o json | jq '.spec.template.spec.containers[] |= (select(.name=="ocm-controller").image = "'"${bastion}"':5000/stolostron/multicloud-manager:deletefix-01")' | oc replace -f -
+oc get deploy -n multicluster-engine ocm-controller -o json | jq '.spec.template.spec.containers[] | select(.name=="ocm-controller").image'
+echo "Sleep 15"
+sleep 15
+
+# Patch MCE managedcluster import controller Memory Limits(Requires MCE pause)
+echo "Patching MCE IBIO container image memory limits"
+oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[0].resources.limits.memory'
+oc get deploy -n multicluster-engine image-based-install-operator -o json |  jq '.spec.template.spec.containers[0].resources.limits.memory = "16Gi"' | oc replace -f -
+oc get deploy -n multicluster-engine image-based-install-operator -o json | jq '.spec.template.spec.containers[0].resources.limits.memory'
