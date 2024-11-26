5673664c977a680dd6259898b015630fd8f31287
Alex Krzos <akrzos@redhat.com>
2024-11-26 15:34:53 -0500

    Rollback can not be combined on IBGU "step" level with "Prep" and "Upgrade"

--- ansible/roles/rhacm-ztp-ibgu-setup/templates/apply-ibgu-rollback.sh
+++ ansible/roles/rhacm-ztp-ibgu-setup/templates/apply-ibgu-rollback.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 {% for ibgu in range(((ztp_done_clusters.stdout_lines | length) / clusters_per_prep_ibgu) | round(0, 'ceil') | int) %}
 date -u
-oc patch ibgu ibgu-{{ seed_image_version | replace('.', '-') }}-{{ '%04d' | format(ibgu) }} -n ztp-platform-upgrade --type=json -p '[{"op": "add", "path": "/spec/plan/-", "value": {"actions": ["Rollback"], "rolloutStrategy": {"maxConcurrency": {{ ibgu_rollback_concurrency }}, "timeout": {{ ibgu_rollback_timeout }} }}}]'
+oc apply -f {{ install_directory }}/rhacm-ztp/ibgu/scripts-{{ seed_image_version | replace('.', '-') }}/ibgu-rollback-{{ seed_image_version | replace('.', '-') }}-{{ '%04d' | format(ibgu) }}.yml
 {% if not loop.last %}
 sleep {{ ibgu_rollback_sleep * 60 }}
 {% endif %}
