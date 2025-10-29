5203c474e2c0c736a3f5831afd653c1930d2c681
Alex <akrzos@users.noreply.github.com>
2025-10-29 10:05:01 -0400

    AAP 2.6 install with EDA (#67)

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ source .venv/bin/activate
 pip3 install --upgrade pip
 # pip3 install ansible-core==2.16.10
 pip3 install -r requirements.txt
-ansible-galaxy collection install --upgrade awx.awx
+ansible-galaxy collection install --upgrade awx.awx ansible.eda
