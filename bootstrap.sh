6c648b9c0ef4afa49ccf504eca298eefbe1deb91
Alex <akrzos@users.noreply.github.com>
2025-10-30 09:01:32 -0400

    ACM deploy graphing tool requires chrome for static image export. Also force image size during write (#73)

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,6 @@ pip3 install --upgrade pip
 # pip3 install ansible-core==2.16.10
 pip3 install -r requirements.txt
 ansible-galaxy collection install --upgrade awx.awx ansible.eda
+
+# Plotly's static image export tool requires Chrome to be installed
+plotly_get_chrome -y
